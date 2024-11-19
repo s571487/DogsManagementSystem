cca176502531bb0c457c05692af89833624e1f6a
Gandla <S571487@nwmissouri.edu>
2024-11-19 12:03:27 -0600

    first commit

---
+++ src/main/java/com/bandiClasses/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,15 @@
+package com.bandiClasses.DMS;
+
+/** @author S571487 [Sudha Sravya Gandla]**/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,11 @@
+package com.bandiClasses.DMS.repository;
+
+/** @author S571487 [Sudha Sravya Gandla]**/
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
